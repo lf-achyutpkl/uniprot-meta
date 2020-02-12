ee73840f27305bf9ca027e346be4d6b53186b975
roshan shrestha <roshan.shrestha4u@gmail.com>
2020-02-12 14:33:16 +0545

    overview design update

--- src/components/protocol-overview.js
+++ src/components/protocol-overview.js
@@ -2,6 +2,7 @@ import { LitElement, html,css } from 'lit-element';
 import '@polymer/paper-button';
 import '@polymer/paper-input/paper-input';
 import '@polymer/paper-input/paper-textarea';
+import '@polymer/paper-input/paper-input-container';
 
 /**
  * `protocol-overview`
@@ -11,6 +12,12 @@ import '@polymer/paper-input/paper-textarea';
  * @polymer
  */
 class ProtocolOverview extends LitElement {
+  static get properties(){
+    return{
+      
+    }
+  }
+
   static get styles(){
     return css `
       .green{
@@ -30,20 +37,38 @@ class ProtocolOverview extends LitElement {
         font-size:20px;
       }
       form{
-        width:calc(80% - 10px);
+  
         margin:0 auto;
+      }
+      .half{
+        width:50%;
       }
     `;
   }
+
+  constructor(){
+    super();
+    this.name = "Lorem ipsum";
+    this.experimentId = "NAX63V9X";
+    this.description = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+    this.experimentNotes = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  }
+
   render() {
     return html`
     <span class="status right">editable</span>
       <h1> Protocol Overview </h1>
       <form>
-        <paper-input class="name-input" label="Name" value="Lorem ipsum"></paper-input>
-        <paper-textarea label="Description" value="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."></paper-textarea>
-        <paper-input class="name-input" label="Experiment ID"></paper-input>
-        <paper-textarea label="Expertiment Notes"></paper-textarea>
+        <paper-input class="name-input" label="Name" value=${this.name}></paper-input>
+        <paper-textarea label="Description" value=${this.description}></paper-textarea>
+        <paper-input-container>
+        <label slot="label">Your name</label>
+     
+         <input is="iron-input" slot="input">
+
+        <!-- <paper-input class="name-input half" label="Experiment ID" value=${this.experimentId} ></paper-input> -->
+        </paper-input-container>
+        <paper-textarea label="Expertiment Notes" value=${this.experimentNotes}></paper-textarea>
         <paper-button raised class="green right">Save</paper-button>
       </form>
     `;
