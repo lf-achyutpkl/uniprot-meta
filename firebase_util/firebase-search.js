04249ae49be275921bc419f54af50908ab9e4406
sumitdhungana14 <sumeetdhungana74@gmail.com>
2020-02-19 12:05:34 +0545

    Search documents by overview name where name like searchString%

--- firebase_util/firebase-search.js
+++ firebase_util/firebase-search.js
@@ -1,6 +1,8 @@
 import metaCollection from './db';
 
+
+//returns all documents that has name field starting with overviewName
 export async function getMetaByOverviewName(overviewName){
 
-    return await metaCollection.where('name', '==' , overviewName).get();
+    return await metaCollection.where('name', '>=' , overviewName).where('name', '<=' , overviewName +'\uf8ff').get();
 }
