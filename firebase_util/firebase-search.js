e746eefd5e160dfdfe4412c8f9031104557ed54a
sumitdhungana14 <sumeetdhungana74@gmail.com>
2020-02-19 12:28:17 +0545

    Limit number of documents returned from firestore.

--- firebase_util/firebase-search.js
+++ firebase_util/firebase-search.js
@@ -1,13 +1,7 @@
 import metaCollection from './db';
-<<<<<<< HEAD
-
 
 //returns all documents that has name field starting with overviewName
-export async function getMetaByOverviewName(overviewName){
+export async function getMetaByOverviewName(overviewName, limitValue){
 
-    return await metaCollection.where('name', '>=' , overviewName).where('name', '<=' , overviewName +'\uf8ff').get();
-=======
-export async function getMetaByOverviewName(overviewName){
-    return await metaCollection.where('name', '==' , overviewName).get();
->>>>>>> 6ecbaeb368b70b69581df35c6a212b9c143fe163
+    return await metaCollection.where('name', '>=' , overviewName).where('name', '<=' , overviewName +'\uf8ff').orderBy('name').limit(limitValue);
 }
