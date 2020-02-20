e6dd80604d1a9dbba6689980b111242223d5f24a
Ishan Subedi <ikushum@gmail.com>
2020-02-20 12:19:45 +0545

    refactor firebase.config file

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD7I3e4jcCWCkpTjDDARhjEOzbV8xpgXpY",
+  authDomain: "my-awesome-project-a149c.firebaseapp.com",
+  databaseURL: "https://my-awesome-project-a149c.firebaseio.com",
+  projectId: "my-awesome-project-a149c",
+  storageBucket: "my-awesome-project-a149c.appspot.com",
+  messagingSenderId: "1048085640248",
+  appId: "1:1048085640248:web:db33ab206c1e0ca4ade2cd",
+  measurementId: "G-S2ZL5Q6XMT"
+};
+
+export default firebaseConfig;
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import firebaseConfig from "../config";
+import firebaseConfig from "../firebase.config";
 
 class FirebaseUtil {
   constructor () {
